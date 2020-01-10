ecd7df29c66d2f87f693d7e402af0b53c4a25b98
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-01-10 12:51:03 +0000

    inizio componente frase

--- spammer/src/app/app.module.ts
+++ spammer/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FraseComponent } from './frase/frase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FraseComponent
   ],
   imports: [
     BrowserModule
