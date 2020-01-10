f758b9524fe2471fbe7a0d5e56d8afde72c73925
Andrea-Favullo <favullo.andrea.dennis@iisgalvanimi.edu.it>
2020-01-10 12:30:46 +0000

    app component quasi finito

--- spammer/src/app/app.component.ts
+++ spammer/src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'spammer';
+  articles = new Array<string>();
+
+  onclick(testo : HTMLInputElement, numero : HTMLInputElement){
+
+    //variabili che immagazinano il valore dei parametri
+    let frase = testo.value;
+    let num = Number(numero.value);
+
+    console.log(`dati ricevuti: ${frase} - ${num}`);
+
+    //ciclo che inserisce la frase num volte nella lista
+    for(let i:number=0; i<num; i++){
+      this.articles.push(frase);
+    }
+
+  }
 }
